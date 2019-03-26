91e91bbfdf988fa73143f231bd1629907855c28f
yc <475117436@qq.com>
2019-03-26 18:27:20 +0800

    travelApp first commit

---
+++ travelApp/src/pages/home/components/Header.vue
@@ -0,0 +1,61 @@
+<!--Home.vue的子组件-->
+<template>
+	<div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      input city or sight
+    </div>
+    <div class="header-right">
+      City
+      <span class="iconfont arrow-icon">&#xe62d;</span>
+    </div>
+	</div>
+</template>
+
+<script>
+
+    export default {
+        name: 'HomeHeader',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--在style里引入另一个css文件需要使用@import 字符前还要加~-->
+<style lang="stylus" type="text/stylus" scoped>
+  @import "~styles/varibles.styl"
+  .header
+    display:flex
+    line-height:0.86rem
+    background: $bgColor
+    color: #f4f4f4
+    .header-left
+      width:.64rem
+      float:left
+      .back-icon
+        text-align center
+        font-size 0.4rem
+  .header-input
+      flex:1
+      height:.64rem
+      line-height: .64rem
+      margin-top .12rem
+      margin-left : .2rem
+      padding-left .2rem
+      background :#ffffff
+      border-radius .15rem
+      color :#cccccc
+  .header-right
+      width 1.24rem
+      float right
+      text-align center
+      .arrow-icon
+        font-size .3rem
+
+
+</style>
---
+++ travelApp/src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<!--Home.vue的子组件-->
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" :alt="item.id"/>
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomeSwiper',
+        data() {
+            return {
+              swiperOption:{
+                pagination: '.swiper-pagination',
+                loop:true
+              },
+              swiperList:[
+                {id:'001',imgUrl:'@/../static/img/5.jpg'},
+                {id:'002 ',imgUrl:'@/../static/img/11.jpg'},
+                {id:'004 ',imgUrl:'@/../static/img/5.jpg'},
+                {id:'005 ',imgUrl:'@/../static/img/4.jpg'}
+              ]
+            }
+        },
+
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only
+.wrapper 的设置使宽高比例固定在56%
+-->
+<style lang="stylus" type="text/stylus" scoped>
+  @import "~styles/varibles.styl"
+  .wrapper>>>.swiper-pagination-bullet-active
+    background :$bgColor
+  .wrapper
+    overflow :hidden
+    width :100%
+    height :0
+    padding-bottom :64%
+    background :#f4f4f4
+    .swiper-img
+      width :100%
+
+</style>
