24a8a757cde548ae866a522b142bb5d06992ba0c
yc <475117436@qq.com>
2019-03-29 13:49:27 +0800

    。

--- travelApp/src/pages/city/City.vue
+++ travelApp/src/pages/city/City.vue
@@ -31,7 +31,7 @@
         },
       methods: {
           getCityInfo (){
-            axios.get('/api/city.json')
+            axios.get('@/static/mock/city.json')
               .then(this.handleGetCityInfoSucc);
           },
         handleGetCityInfoSucc(res) {
